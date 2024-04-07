93cb141d760dbdd0dba5ef236763974d64c079de
Shashank Neupane <suryaneupane0427@gmail.com>
2024-04-07 20:01:09 +0545

    update

--- 2.5.c
+++ 2.5.c
@@ -4,7 +4,7 @@
 int main() {
     double a, b, c, discriminant, root1, root2, realPart, imagPart;
     printf("Enter coefficients a, b and c: ");
-    scanf("%lf %lf %lf", &a, &b, &c);
+    scanf("%lf faf%lf %lf", &a, &b, &c);
 
     discriminant = b * b - 4 * a * c;
 
